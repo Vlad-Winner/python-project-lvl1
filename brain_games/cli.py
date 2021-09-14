d8dbcb815a8c0508f45b8a6e06fba2f5911cd10f
Vladislav Kozyrev <vladwinnergame@gmail.com>
2021-09-14 12:56:06 +0700

    fix linter

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,5 +1,8 @@
+"""Client action."""
 import prompt
 
+
 def welcome_user():
+    """User identification."""
     name = prompt.string('May I have your name? ')
-    print('Hello, {}!'.format(name))
+    print('Hello, {0}!'.format(name))
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
+"""Main module."""
 from brain_games.cli import welcome_user
 
+
 def main():
+    """Start the game."""
     print('Welcome to the Brain Games!')
     welcome_user()
-
 
 
 if __name__ == '__main__':
