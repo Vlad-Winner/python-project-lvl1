90f23cd7010d9559e76090e8ded92097f0561acc
Vladislav Kozyrev <vladwinnergame@gmail.com>
2021-09-29 02:36:10 +0700

    fix brain-progression

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -1,35 +1,24 @@
-#!/usr/bin/env python
 from random import randrange
-from brain_games.scripts import logic
 
 
-def main():
-    name = logic.welcome()
-    print('What number is missing in the progression?')
-    win = 0
-    for i in range(3):
-        sequence_amount = randrange(5, 11)
-        step = randrange(2, 11)
-        missing_number = randrange(sequence_amount)
-        start_sequence = randrange(2, 30)
-        question_array = [start_sequence]
-        for j in range(sequence_amount - 1):
-            question_array.append(question_array[j] + step)
-        question_array[missing_number], answer = (
-            '..',
-            str(question_array[missing_number]))
-        question = ''
-        for k in range(len(question_array)):
-            if k < len(question_array) - 1:
-                question += str(question_array[k]) + ' '
-            else:
-                question += str(question_array[k])
-        if logic.run(question, answer, name):
-            win += 1
+RULES = 'What number is missing in the progression?'
+NUMBER_COUNT = 1
+LIMIT = 30
+
+
+def main(question_array):
+    sequence_amount = randrange(5, 11)
+    step = randrange(2, 11)
+    missing_number = randrange(sequence_amount)
+    for i in range(sequence_amount - 1):
+        question_array.append(question_array[i] + step)
+    question_array[missing_number], answer = (
+        '..',
+        str(question_array[missing_number]))
+    question = ''
+    for i in range(len(question_array)):
+        if i < len(question_array) - 1:
+            question += str(question_array[i]) + ' '
         else:
-            break
-    logic.finish(win, name)
-
-
-if __name__ == '__main__':
-    main()
+            question += str(question_array[i])
+    return question, answer
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from brain_games.scripts import logic
+from brain_games.games import brain_progression as progression
+
+
+def main():
+    logic.run(progression)
+
+
+if __name__ == '__main__':
+    main()
