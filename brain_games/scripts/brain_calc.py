2e5f8cb4d719e6346734147d7562b06707191fbd
Vladislav Kozyrev <vladwinnergame@gmail.com>
2021-09-29 01:59:54 +0700

    fix brain-calc

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from brain_games.games import brain_calc as calc
+from brain_games.scripts import logic
+
+
+def main():
+    logic.run(calc)
+
+
+if __name__ == '__main__':
+    main()
