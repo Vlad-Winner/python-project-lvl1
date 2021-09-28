3aad732ecce16f608bc30e2fa8d3273e6de9d125
Vladislav Kozyrev <vladwinnergame@gmail.com>
2021-09-29 01:49:16 +0700

    fix games/brain_even.py

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -1,18 +1,13 @@
-#!/usr/bin/env python
 """Game: Parity check."""
 RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
 NUMBER_COUNT = 1
 LIMIT = 21
 
 
-def question(number):
-    return number[0]
-
-
-def right_answer(number):
+def main(number):
     number = number[0]
     if number % 2 == 0:
         answer = 'yes'
     else:
         answer = 'no'
-    return answer
+    return number, answer
