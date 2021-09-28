0eda180665d40b4a199b7814d9607e593e7aee92
Vladislav Kozyrev <vladwinnergame@gmail.com>
2021-09-29 00:38:02 +0700

    fix brain-even

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -1,26 +1,18 @@
 #!/usr/bin/env python
 """Game: Parity check."""
-from brain_games.scripts import logic
-from random import randrange
+RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
+NUMBER_COUNT = 1
+LIMIT = 21
 
 
-def main():
-    """Start the game."""
-    name = logic.welcome()
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    win = 0
-    for i in range(3):
-        number = randrange(1, 21)
-        if number % 2 == 0:
-            answer = 'yes'
-        else:
-            answer = 'no'
-        if logic.run(number, answer, name):
-            win += 1
-        else:
-            break
-    logic.finish(win, name)
+def question(number):
+    return number[0]
 
 
-if __name__ == '__main__':
-    main()
+def right_answer(number):
+    number = number[0]
+    if number % 2 == 0:
+        answer = 'yes'
+    else:
+        answer = 'no'
+    return answer
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,10 @@
+from brain_games.scripts import logic
+from brain_games.games import brain_even as even
+
+
+def main():
+    logic.run(even)
+
+
+if __name__ == '__main__':
+    main()
