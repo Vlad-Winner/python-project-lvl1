1ea79ba2a2839f235446de37f3e363a098bebd87
Vladislav Kozyrev <vladwinnergame@gmail.com>
2021-10-04 22:03:08 +0700

    fix games/brain_progression.py

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -20,19 +20,10 @@ def get_arithmetic_series():
     return arithmetic_series
 
 
-def convert_arithmetic_series_to_string(arithmetic_series):
-    arithmetic_series_str = ''
-    for i in range(len(arithmetic_series)):
-        if i < len(arithmetic_series) - 1:
-            arithmetic_series_str += str(arithmetic_series[i]) + ' '
-        else:
-            arithmetic_series_str += str(arithmetic_series[i]) + ' '
-    return arithmetic_series_str
-
-
 def get_question_and_answer():
     arithmetic_series = get_arithmetic_series()
     missing_number = randrange(len(arithmetic_series))
     answer = str(arithmetic_series[missing_number])
     arithmetic_series[missing_number] = '..'
-    return convert_arithmetic_series_to_string(arithmetic_series), answer
+    question = ' '.join(map(str, arithmetic_series))
+    return question, answer
