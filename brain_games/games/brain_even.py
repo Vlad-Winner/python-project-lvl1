ffe723417d9ba4eeb031daa19f0166d483e9e8cd
Vladislav Kozyrev <vladwinnergame@gmail.com>
2021-10-04 20:27:56 +0700

    fix games/brain_even.py

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -3,10 +3,12 @@ from random import randrange
 
 
 RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
+NUMBER_START = 1
+NUMBER_LIMIT = 21
 
 
 def get_question_and_answer():
-    question = randrange(1, 21)
+    question = randrange(NUMBER_START, NUMBER_LIMIT)
     if question % 2 == 0:
         answer = 'yes'
     else:
