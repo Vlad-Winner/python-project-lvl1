93d3dd280e2c0d1911407b44cfa96909671f64df
Vladislav Kozyrev <vladwinnergame@gmail.com>
2021-10-04 20:33:02 +0700

    fix games/brain_gcd.py

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -3,6 +3,8 @@ from random import randrange
 
 
 RULES = 'Find the greatest common divisor of given numbers.'
+NUMBER_START = 1
+NUMBER_LIMIT = 50
 
 
 def get_gcd(one, two):
@@ -15,9 +17,8 @@ def get_gcd(one, two):
 
 
 def get_question_and_answer():
-    numbers = []
-    for i in range(2):
-        numbers.append(randrange(1, 50))
-    question = str(numbers[0]) + ' ' + str(numbers[1])
-    answer = str(get_gcd(numbers[0], numbers[1]))
+    number_first = randrange(NUMBER_START, NUMBER_LIMIT)
+    number_second = randrange(NUMBER_START, NUMBER_LIMIT)
+    question = str(number_first) + ' ' + str(number_second)
+    answer = str(get_gcd(number_first, number_second))
     return question, answer
