88e172b68d16ab29368e8a320c04601a32f93129
Vladislav Kozyrev <vladwinnergame@gmail.com>
2021-10-04 21:47:43 +0700

    fix engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -9,18 +9,15 @@ def run(game):
     name = prompt.string('May I have your name? ')
     print('Hello, {}!'.format(name))
     print(game.RULES)
-    win = 0
-    for i in range(ROUND_COUNT):
+    for _ in range(ROUND_COUNT):
         question, right_answer = game.get_question_and_answer()
         print('Question: ' + str(question))
         user_answer = prompt.string('Your answer: ')
         if user_answer == right_answer:
             print('Correct!')
-            win += 1
         else:
             print("'{}' is wrong answer ;(. "
                   "Correct answer was '{}'".format(user_answer, right_answer))
             print("Let's try again, {}!".format(name))
-            break
-    if win == ROUND_COUNT:
-        print('Congratulations, {}!'.format(name))
+            return
+    print('Congratulations, {}!'.format(name))
