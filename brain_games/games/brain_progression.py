dc45f265546658c1a5bf1ac645e3706474af4266
Vladislav Kozyrev <vladwinnergame@gmail.com>
2021-10-04 21:43:37 +0700

    fix games/brain_progression.py

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -2,12 +2,18 @@ from random import randrange
 
 
 RULES = 'What number is missing in the progression?'
+SERIES_AMOUNT_BEGIN = 5
+SERIES_AMOUNT_LIMIT = 11
+DIFFERENCE_BEGIN = 2
+DIFFERENCE_LIMIT = 16
+FIRST_NUMBER_BEGIN = 2
+FIRST_NUMBER_LIMIT = 30
 
 
 def get_arithmetic_series():
-    series_amount = randrange(5, 11)
-    difference = randrange(2, 16)
-    first_number = randrange(2, 30)
+    series_amount = randrange(SERIES_AMOUNT_BEGIN, SERIES_AMOUNT_LIMIT)
+    difference = randrange(DIFFERENCE_BEGIN, DIFFERENCE_LIMIT)
+    first_number = randrange(FIRST_NUMBER_BEGIN, FIRST_NUMBER_LIMIT)
     arithmetic_series = [first_number]
     for i in range(series_amount):
         arithmetic_series.append(arithmetic_series[i] + difference)
@@ -25,9 +31,8 @@ def convert_arithmetic_series_to_string(arithmetic_series):
 
 
 def get_question_and_answer():
-    question = get_arithmetic_series()
-    missing_number = randrange(len(question))
-    question[missing_number], answer = (
-        '..',
-        str(question[missing_number]))
-    return convert_arithmetic_series_to_string(question), answer
+    arithmetic_series = get_arithmetic_series()
+    missing_number = randrange(len(arithmetic_series))
+    answer = str(arithmetic_series[missing_number])
+    arithmetic_series[missing_number] = '..'
+    return convert_arithmetic_series_to_string(arithmetic_series), answer
